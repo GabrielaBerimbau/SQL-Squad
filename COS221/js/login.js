51b91adbcd41a64a874d9972b365d4458a5655f9
Kahlan Hagerman <kahlanhagerman@gmail.com>
2025-05-20 13:01:20 +0200

    file creation and start

---
+++ COS221/js/login.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function(){
+    const loginForm = document.getElementById('login-form');
+    const errorMessage = document.getElementById('error-msg');
+
+    loginForm.addEventListener('submit', function(e){
+        
+    })
+})
