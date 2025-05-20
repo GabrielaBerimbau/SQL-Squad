4eb087b5c4d402471b04bb63101b02e7a1dcda20
Kahlan Hagerman <kahlanhagerman@gmail.com>
2025-05-20 13:02:55 +0200

    checking username & password entered

--- COS221/js/login.js
+++ COS221/js/login.js
@@ -3,6 +3,19 @@ document.addEventListener('DOMContentLoaded', function(){
     const errorMessage = document.getElementById('error-msg');
 
     loginForm.addEventListener('submit', function(e){
-        
+        e.preventDefault();
+
+        const username = document.getElementById('username').value.trim();
+        const password = document.getElementById('password').value;
+
+        if(!username || !password){
+            showError('Please enter a username and password');
+            return;
+        }
+
+        const formData = new FormData();
+
+        formData.append('username', username);
+        formData.append('password', password);
     })
 })
