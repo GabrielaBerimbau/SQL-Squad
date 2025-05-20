f21fe457a2c362c02f5610733a7802ce9ac1955e
Megan Azmanov <meganazmanov@gmail.com>
2025-05-20 13:17:50 +0200

    filtering

--- COS221/js/products.js
+++ COS221/js/products.js
@@ -56,3 +56,58 @@ function fetchProducts() {
 
     xhr.send(JSON.stringify(requestData));
 }
+
+
+
+/**
+ * Populates filter dropdowns with available options from products
+ */
+
+
+function populateFilters(){
+    const categories = new Set();
+    const brands = new Set();
+
+    products.forEach(product => {
+        if (product.category_id) categories.add(product.category_id);
+        if (product.brand) brands.add(product.brand);
+    });
+    // Populate category dropdown
+    populateDropdown("category-select", categories);
+    
+    // Populate brand dropdown
+    populateDropdown("brand-select", brands);
+    
+    // Set up price range
+    const priceRange = document.getElementById("price-range");
+    if (priceRange) {
+        priceRange.addEventListener("input", function() {
+            document.getElementById("price-range-value").textContent = `Max Price: R${this.value}`;
+            updateProductDisplay();
+        });
+    }
+}
+
+
+/**
+ * Populates a dropdown with options
+ * @param {string} id - The ID of the select element
+ * @param {Set} options - Set of options to add
+ */
+function populateDropdown(id, options) {
+    const select = document.getElementById(id);
+    if (!select) return;
+    
+    // Keep the default option
+    const defaultOption = select.querySelector("option");
+    select.innerHTML = "";
+    if (defaultOption) select.appendChild(defaultOption);
+    
+    // Add options
+    options.forEach(option => {
+        const opt = document.createElement("option");
+        opt.value = option;
+        opt.textContent = option;
+        select.appendChild(opt);
+    });
+}
