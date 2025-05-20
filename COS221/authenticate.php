a85a20c9e308b091f256a20a5bc8127d2b183d00
Kahlan Hagerman <kahlanhagerman@gmail.com>
2025-05-20 13:35:43 +0200

    .

--- COS221/authenticate.php
+++ COS221/authenticate.php
@@ -34,6 +34,7 @@
                     exit;
                 }
 
+                // verifying hashed passwords here
                 if(password_verify($password, $user['password'])){
                     $_SESSION['user_id'] = $user['user_id'];
                     $_SESSION['username'] = $username;
